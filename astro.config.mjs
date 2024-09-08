60dbe07266b472ef5a392a52c98fe603c80ef94c
light-souler <light.souler@hotmail.com>
2024-09-08 16:30:41 +0700

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://light-souler.github.io",
   base: "/",
   trailingSlash: "always",
   integrations: [
